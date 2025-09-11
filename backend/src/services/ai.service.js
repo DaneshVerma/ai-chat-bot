c3523e808760cc9117fc8781097b2035bb41368f
Danesh <techydanesh@gmail.com>
2025-09-11 18:53:01 +0530

    implemented short term memory to ai  to remeber recent coversitation for context

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -4,6 +4,8 @@ const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
 
+const memory = [];
+
 async function generateResult(prompt) {
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
@@ -14,13 +16,31 @@ async function generateResult(prompt) {
 }
 
 async function generateStreamResponse(prompt, callBaclkFn) {
-  const response = await ai.models.generateContentStream({
-    model: "gemini-2.0-flash",
-    contents: prompt,
+  memory.push({
+    role: "user",
+    parts: [
+      {
+        text: prompt,
+      },
+    ],
   });
-  for await (const chunks of response) {
+  const stream = await ai.models.generateContentStream({
+    model: "gemini-2.0-flash",
+    contents: memory,
+  });
+  let responsetext = "";
+  for await (const chunks of stream) {
+    responsetext += chunks.text;
     callBaclkFn(chunks.text);
   }
+  memory.push({
+    role: "model",
+    parts: [
+      {
+        text: responsetext,
+      },
+    ],
+  });
 }
 
 module.exports = {
