32891bb7f8fbff10a5f713fe7bff09a68d5a62c0
Danesh <techydanesh@gmail.com>
2025-09-11 18:31:53 +0530

    added authentication middleware in socket.io

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -1,10 +1,31 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
 
 function initSocket(httpServer) {
   const io = new Server(httpServer);
 
   io.on("connection", (socket) => {
     console.log("A user connected");
+    io.use((socket, next) => {
+      const cookies = socket.handshake.headers.cookie;
+
+      const { token } = cookies ? cookie.parse(cookies) : {};
+
+      if (!token) {
+        return next(new Error("Authentication error"));
+      }
+
+      try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        socket.user = decoded;
+
+        next();
+      } catch (err) {
+        return next(new Error("Invalid token"));
+      }
+    });
 
     socket.on("disconnect", () => {
       console.log("A user disconnected");
