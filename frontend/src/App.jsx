5e1db12112a2058538964005083c756c161ec945
Danesh <techydanesh@gmail.com>
2025-09-15 11:38:22 +0530

    frontend part completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Routes, Route, Navigate, } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home.jsx'
+import Register from './pages/Register.jsx'
+import Login from './pages/Login.jsx'
 
-const App = () => {
+function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<h2 style={{ padding: '2rem' }}>404 - Not Found</h2>} />
+      </Routes>
     </div>
   )
 }
